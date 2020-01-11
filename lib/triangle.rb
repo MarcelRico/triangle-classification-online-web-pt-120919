7ac2e98d080bbb38c2afc13a5cad50e898fa87e2
Marcel Rico <personal.mr4@gmail.com>
2020-01-11 22:05:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side1,:side2,:side3
+
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def is_scalene?
+    (@side1 != @side2) && (@side2  != @side3) && (@side1 != @side3)
+  end
+  
+  def is_iso?
+    (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+  end
+  
+  def is_equi?
+    (@side1 == @side2) && (@side2 == @side3) && (@side1 == @side3)
+  end
+  
+  def kind
+    
+    
+    if is_equi?
+      return :equilateral
+    elsif is_iso?
+      return :isosceles
+    elsif is_scalene?
+      return :scalene
+    end
+  
+  class TriangleError < StandardError
+  end
+  
 end
